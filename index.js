f7ad6ef4012f93064b83d89ecbf9e17ef9a060ae
jafow <jared.a.fowler@gmail.com>
2017-02-07 14:23:29 -0800

    Paint axis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* global d3 data */
-var d = data
+// var d = data
 var svg = d3.select('svg')
 var margin = { top: 20, right: 20, bottom: 20, left: 20 }
 var width = Number(svg.attr('width')) - margin.left - margin.right
@@ -12,12 +12,36 @@ var y = d3.scaleLinear().rangeRound([height, 0])
 
 var l = d3.line()
   .x(function (d) { return x(d.date) })
-  .y(function (d) { return y(d.output.bm_count) })
+  .y(function (d) { return y(d.output) })
 
-x.domain(d3.extent(d, (_d) => d.date))
-y.domain(d3.extent(d, (_d) => d.output.bm_count))
 
-g.data(d)
-  .enter()
-  .append('g')
+d3.json('data.json', function (err, _data) {
+  if (err) throw err
 
+  x.domain(d3.extent(_data, (d) => d.date))
+  y.domain(d3.extent(_data, (d) => Number(d.output.bm_count)))
+
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove()
+
+  g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+
+  g.append("path")
+    .datum(_data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", l)
+})
